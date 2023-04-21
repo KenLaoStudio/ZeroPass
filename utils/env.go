6d2a0a54032d44f66008fa82ba1b149f3a0ca69c
MorrisLin <aka611511@gmail.com>
2023-04-22 00:19:54 +0800

    feat: add .env

---
+++ utils/env.go
@@ -0,0 +1,17 @@
+package utils
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func GetEnv(key string) string {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	return os.Getenv(key)
+}
---
+++ utils/env_test.go
@@ -0,0 +1,32 @@
+package utils
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestGetEnv(t *testing.T) {
+	type args struct {
+		key string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		// TODO: Add test cases.
+		// add test cases
+		{
+			name: "DB_CONNECTION",
+			args: args{
+				key: "DB_CONNECTION",
+			},
+			want: "mongodb://zeroPass:9674asdasdhzx&T7@127.0.0.1:27017/?retryWrites=true&w=majority",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equalf(t, tt.want, GetEnv(tt.args.key), "GetEnv(%v)", tt.args.key)
+		})
+	}
+}
